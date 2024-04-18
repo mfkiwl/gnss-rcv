ef40b6608ece26886536f20fad5ff933e42a10eb
mx4 <mx4@users.noreply.github.com>
2024-04-18 22:36:45 +0000

    add option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "gnss-test", about = "gnss tester")]
+struct Options {
+    #[structopt(long, default_value = "raw-iq.dat")]
+    img_file: PathBuf,
+    #[structopt(short = "x", long, default_value = "0")]
+    res_x: u32,
+    #[structopt(short = "y", long, default_value = "0")]
+    res_y: u32,
+}
+
 fn main() {
     println!("gnss-test");
 }
