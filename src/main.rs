3507c2a97e14c7b068f02d1f175a14e5b74c6830
mx4 <mx4@users.noreply.github.com>
2024-04-18 22:36:58 +0000

    add option

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
+use colored::Colorize;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "gnss-test", about = "gnss tester")]
 struct Options {
     #[structopt(long, default_value = "raw-iq.dat")]
-    img_file: PathBuf,
+    file: PathBuf,
     #[structopt(short = "x", long, default_value = "0")]
-    res_x: u32,
+    x: u32,
     #[structopt(short = "y", long, default_value = "0")]
-    res_y: u32,
+    y: u32,
 }
 
 fn main() {
-    println!("gnss-test");
+    let opt = Options::from_args();
+
+    println!("gnss-test: file: {} - x={} y={}", opt.file.display(), opt.x, opt.y);
 }
