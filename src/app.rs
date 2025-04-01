d81c841093b51b6e2539e371a423900fc3f94a32
Maxime Austruy <austruym@gmail.com>
2025-04-02 00:17:59 +0200

    improve ui layout

--- src/app.rs
+++ src/app.rs
@@ -1,17 +1,19 @@
+use egui_extras::{Column, TableBuilder};
+use egui_extras::{Size, StripBuilder};
 use log::info;
 use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 
-const WIDTH: usize = 600;
+const WIDTH: usize = 800;
 const HEIGHT: usize = 600;
-const SIDE_PANEL_WIDTH: usize = 250;
 
 pub struct GnssRcvApp {
     iq_file: String,
     iq_file_choice: usize,
     iq_type_choice: usize,
     needs_stop: Arc<AtomicBool>,
+    active: bool
 }
 
 impl Default for GnssRcvApp {
@@ -20,6 +22,7 @@ impl Default for GnssRcvApp {
             iq_file: "resources/nov_3_time_18_48_st_ives".to_owned(),
             iq_file_choice: 0,
             iq_type_choice: 0,
+            active: false,
             needs_stop: Arc::new(AtomicBool::new(false)),
         }
     }
@@ -35,7 +38,7 @@ impl GnssRcvApp {
         info!("stop_async");
     }
 
-    fn start_async(&mut self, _ctx: &egui::Context) {
+    fn start_async(&mut self) {
         info!("start_async");
     }
 }
@@ -44,7 +47,7 @@ pub fn egui_main() {
     info!("egui_main");
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([(SIDE_PANEL_WIDTH + WIDTH + 50) as f32, (HEIGHT + 50) as f32]),
+            .with_inner_size([WIDTH as f32, HEIGHT as f32]),
         ..eframe::NativeOptions::default()
     };
     eframe::run_native(
@@ -58,15 +61,13 @@ pub fn egui_main() {
 impl eframe::App for GnssRcvApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let vec_str = [
-            "nov_3_time_18_48_st_ives".to_owned(),
-            "gpssim_2xi16".to_owned(),
-            "L1_20211202_084700_4MHz_IQ.bin".to_owned(),
-            "GPS-L1-2022-03-27.sigmf-data".to_owned(),
+            "nov_3_time_18_48_st_ives",
+            "gpssim_2xi16",
+            "L1_20211202_084700_4MHz_IQ.bin",
+            "GPS-L1-2022-03-27.sigmf-data",
         ];
-        let type_str = [
-           "2xf32".to_owned(),
-           "2xi16".to_owned(),
-        ];
+        let type_str = ["2xf32", "2xi16"];
+
         egui::TopBottomPanel::top("top_panel")
             .resizable(false)
             .min_height(25.0)
@@ -74,10 +75,11 @@ impl eframe::App for GnssRcvApp {
                 egui::Grid::new("TopGrid").show(ui, |ui| {
                     egui::ComboBox::from_label("Pick file")
                         .width(230.0)
-                        .selected_text(vec_str[self.iq_file_choice].clone())
+                        .selected_text(vec_str[self.iq_file_choice])
                         .show_ui(ui, |ui| {
                             for (i, s) in vec_str.iter().enumerate() {
-                                let value = ui.selectable_value(&mut self.iq_file_choice, i, s);
+                                let value =
+                                    ui.selectable_value(&mut self.iq_file_choice, i, s.to_string());
                                 if value.clicked() {
                                     self.iq_file_choice = i;
                                     self.iq_file = format!("resources/{}", vec_str[i]);
@@ -93,26 +95,35 @@ impl eframe::App for GnssRcvApp {
                     });
                     ui.horizontal(|ui| {
                         egui::ComboBox::from_label("type")
-                        .width(30.0)
-                        .selected_text(type_str[self.iq_type_choice].clone())
-                        .show_ui(ui, |ui| {
-                            for (i, s) in type_str.iter().enumerate() {
-                                let value = ui.selectable_value(&mut self.iq_type_choice, i, s);
-                                if value.clicked() {
-                                    self.iq_type_choice = i;
+                            .width(30.0)
+                            .selected_text(type_str[self.iq_type_choice])
+                            .show_ui(ui, |ui| {
+                                for (i, s) in type_str.iter().enumerate() {
+                                    let value = ui.selectable_value(
+                                        &mut self.iq_type_choice,
+                                        i,
+                                        s.to_string(),
+                                    );
+                                    if value.clicked() {
+                                        self.iq_type_choice = i;
+                                    }
                                 }
-                            }
-                        });
+                            });
                     });
                     ui.end_row();
+                    let button_text = if self.active { "stop" } else { "start" };
+                    if ui.add_sized([80.0, 25.],
+                                egui::Button::new(button_text.to_owned())).clicked() {
+                        if self.active {
+                            self.stop_async();
+                            self.active = false;
+                        } else {
+                            self.start_async();
+                            self.active = true;
+                        }
+                    }
                 });
             });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                ui.heading("PRN info");
-            });
-        });
 
         egui::TopBottomPanel::bottom("bottom_panel")
             .resizable(true)
@@ -121,6 +132,78 @@ impl eframe::App for GnssRcvApp {
                 egui::ScrollArea::vertical().show(ui, |ui| {
                     ui.heading("log file here");
                 });
+            });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                StripBuilder::new(ui)
+                    .size(Size::remainder().at_least(100.0)) // for the table
+                    .vertical(|mut strip| {
+                        strip.cell(|ui| {
+                            egui::ScrollArea::horizontal().show(ui, |ui| {
+                                self.table_ui(ui);
+                            });
+                        });
+                    });
+            });
+        });
+    }
+}
+
+impl GnssRcvApp {
+    fn table_ui(&mut self, ui: &mut egui::Ui) {
+        let available_height = ui.available_height();
+        let table = TableBuilder::new(ui)
+            .resizable(true)
+            .striped(true)
+            .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+            .column(Column::auto())
+            .column(Column::auto().at_least(30.0).resizable(true))
+            .column(Column::auto())
+            .column(Column::remainder())
+            .column(Column::remainder())
+            .min_scrolled_height(0.0)
+            .max_scroll_height(available_height);
+
+        table
+            .header(20.0, |mut header| {
+                header.col(|ui| {
+                    ui.strong("PRN");
+                });
+                header.col(|ui| {
+                    ui.strong("dB-Hz");
+                });
+                header.col(|ui| {
+                    ui.strong("detail-0");
+                });
+                header.col(|ui| {
+                    ui.strong("detail-1");
+                });
+                header.col(|ui| {
+                    ui.strong("detail-2");
+                });
+            })
+            .body(|mut body| {
+                for row_index in 0..32 {
+                    let row_height = 20.0;
+                    body.row(row_height, |mut row| {
+                        row.col(|ui| {
+                            ui.label(row_index.to_string());
+                        });
+                        row.col(|ui| {
+                            ui.label(format!("10 dB-Hz"));
+                        });
+                        row.col(|ui| {
+                            ui.label(format!("xyz"));
+                        });
+                        row.col(|ui| {
+                            ui.label(".-_-_-.");
+                        });
+                        row.col(|ui| {
+                            ui.label(format!("oOoOo"));
+                        });
+                    });
+                }
             });
     }
 }
